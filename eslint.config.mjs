55061669276e94a856f0a1307f4d86be53e58c8b
Pete Gadomski <pete.gadomski@gmail.com>
2025-05-19 06:00:18 -0600

    fix: config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
   }),
 ];
 
--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.PAGES_BASE_PATH,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
