f829160438c192a2b4c3a8c7bdd2240579e4cd31
Pete Gadomski <pete.gadomski@gmail.com>
2025-05-20 08:05:28 -0600

    feat: wasm

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
     },
   }),
